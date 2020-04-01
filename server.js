b610799374ffd2616a8a930f0660bfbe18771cc1
Hansanghyeon <dandan9509@gmail.com>
2020-04-01 19:54:57 +0900

    feat: meta date 가져오기

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
-'use strict';
-
 const express = require('express');
-
-// 상수
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// 앱
+const puppeteer = require('puppeteer');
 const app = express();
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('🔥Meta Crawler');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/:function', (req, res) => {
+  switch (req.params.function) {
+    case 'seo':
+      const {url} = req.query;
+      (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+
+        const meta = await page.$$eval(`meta[property*='og:']`,data => data.map(d => {
+          let data = {};
+          const keyRegExp = new RegExp('og:(.+)');
+          const key = keyRegExp.exec(d.getAttribute('property'));
+          const value = d.getAttribute('content');
+          data[key[1]] = value;
+          return data;
+        }));
+        console.log(meta);
+        meta.forEach(async og => {
+        })
+        // if (meta.hasOwnPropery('images')) {
+        // }
+        await browser.close();
+        return meta;
+      })();
+    default: break;
+  }
+  
+  res.send('🔥Meta Crawler');
+});
+
+app.listen(process.env.PORT || 3000);

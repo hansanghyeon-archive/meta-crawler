92aa8e6f2717351ab4957556ebbab34879d1286d
999hyeon <dandan9509@gmail.com>
2020-04-06 23:22:29 +0900

    feat: Docker image CORS origin allow 셋팅 (#9)
    * chore(package): dotenv & cors
    
    * feat: api 서버 origin 설정
    
    * feat: deploy 모드설정및 도커이미지 배포 1.0.4
    
    * chore(version): package.json 적용

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
 const { createSeoData } = require('./createSeoData');
 const app = express();
+
+dotenv.config({ path: path.join(__dirname, '.env') });
+
+app.all('/*', (req, res, next) => {
+  console.log(process.env.NODE_ENV);
+  res.header(
+    'Access-Control-Allow-Origin',
+    process.env.NODE_ENV === 'production' ? 'https://4log.hapas.io/' : '*',
+  );
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('🔥Meta Crawler');
